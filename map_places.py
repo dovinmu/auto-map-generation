32b974895aa21af129c9b973d7a8e7915e962fd1
Rowan Copley <dovinmu@gmail.com>
2019-01-30 17:59:37 -0500

    More location data for mapping

--- map_places.py
+++ map_places.py
@@ -2,9 +2,15 @@ import geopandas as gpd
 import pandas as pd
 from shapely.geometry import Point
 import matplotlib.pyplot as plt
+import matplotlib.pylab as pylab
 import geopandas
+import numpy as np
+from scipy import ndimage
+
+world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
 
 def getDataFrame(latlngs):
+    print("getDataFrame DEPRECATED, use makeDataFrame instead")
     df = pd.DataFrame(list(latlngs.values()), index=list(latlngs.keys()), columns=['Coordinates'])
 
     gdf = gpd.GeoDataFrame(df, geometry='Coordinates')
@@ -13,8 +19,6 @@ def getDataFrame(latlngs):
 def getBoundingBox(gdf):
     return { 'xmax': 10, 'xmin': -10, 'ymax': 53, 'ymin': 45}
 
-
-
 def mapCategory(gdf, ax, color):
     gdf.plot(ax=ax, color=color)
     for idx,row in gdf.iterrows():
@@ -22,3 +26,40 @@ def mapCategory(gdf, ax, color):
         plt.annotate(s=idx, xy=text_coords, horizontalalignment='center', size="medium")
 
     return ax
+
+def heatmap(d, bins=(400,400), smoothing=3, cmap='Greys'):
+    def getx(pt):
+        return pt.coords[0][0]
+
+    def gety(pt):
+        return pt.coords[0][1]
+    ax = world.plot(
+        color='white', edgecolor='black', figsize= (12,12), alpha=0.15
+    )
+
+    x = list(d.geometry.apply(getx))
+    y = list(d.geometry.apply(gety))
+    heatmap, xedges, yedges = np.histogram2d(y, x, bins=bins)
+    extent = [yedges[0], yedges[-1], xedges[-1], xedges[0]]
+    #extent = (-180,180,-90,90)
+    logheatmap = np.log(heatmap)
+    logheatmap[np.isneginf(logheatmap)] = 0
+    logheatmap = ndimage.filters.gaussian_filter(logheatmap, smoothing, mode='nearest')
+
+    plt.imshow(logheatmap, cmap=cmap, extent=extent)
+    #plt.colorbar()
+    plt.gca().invert_yaxis()
+
+    plt.xlim(-120, 50)
+    plt.ylim(-75, 75)
+    plt.show()
+
+def mapByCategory(gdf):
+    ax = world.plot(
+        color='white', edgecolor='black', figsize=(20,20)
+    )
+    colors = ['red', 'orange', 'green', 'blue', 'indigo', 'purple', 'brown']
+    for i,cat in enumerate(gdf.category.unique()):
+        gdf[gdf.category == cat].plot(ax=ax, color=colors[i%len(colors)], label=cat, alpha=0.25)
+    plt.legend()
+    plt.show()
--- text_to_places.py
+++ text_to_places.py
@@ -1,27 +1,57 @@
 import spacy
 import en_core_web_sm
 import geocoder
+import pandas as pd
+import geopandas as gpd
 from shapely.geometry import Point
 nlp = en_core_web_sm.load()
 
 skip = ['mars', 'earth', '\n', 'xi', 'xv']
+current_id = 0
 
-def getLocations(text):
-    return getLabeledEntities(text, ['GPE', 'LOC'])
+def getLocations(text, nameOnly=True):
+    return getLabeledEntities(text, ['GPE', 'LOC'], nameOnly)
 
-def getLabeledEntities(text, labels):
+def getLabeledEntities(text, labels, nameOnly=True):
     doc = nlp(text)
-    locations = [X for X in doc.ents if X.label_ in labels and X.text.lower() not in skip]
-    return locations
+    entities = []
+    for i,X in enumerate(doc.ents):
+        if X.label_ in labels and X.text.lower() not in skip:
+            if nameOnly:
+                entities.append(X.text)
+            else:
+                entities.append([X.text, X.label_, i, X.sent])
+    return entities
 
 def getLatLng(locations):
+    coordinates = {}
+    for i,X in enumerate(locations):
+        if type(X) == type([]):
+            location_name = X[0]
+        else:
+            location_name = X
+        print(f'{i+1}/{len(locations)}', location_name)
+        if location_name not in coordinates:
+            coordinates[location_name] = geocoder.osm(location_name.lower())
 
-    coordinates = { X.text: geocoder.osm(X.text.lower()) for X in locations}
-    latlngs = {}
+    latLngs = {}
     for name,item in coordinates.items():
         if item.geojson['features']:
-            latlngs[name] = Point(
+            latLngs[name] = Point(
                 item.geojson['features'][0]['properties']['lng'],
                 item.geojson['features'][0]['properties']['lat']
             )
-    return latlngs
+    return latLngs
+
+def makeDataFrame(text):
+    locs = getLocations(text, False)
+    latLngs = getLatLng(locs)
+    filtered_locs = []
+    for loc in locs:
+        if loc[0] in latLngs:
+            filtered_locs.append(loc + [latLngs[loc[0]]])
+
+    df = pd.DataFrame(filtered_locs, columns=['text', 'label', 'idx', 'sentence', 'coordinates'])
+
+    gdf = gpd.GeoDataFrame(df, geometry='coordinates')
+    return gdf
