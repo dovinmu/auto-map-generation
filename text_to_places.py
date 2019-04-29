7e17522b437a681ebca0679b16c7b0a2de2d38e7
Rowan Copley <dovinmu@gmail.com>
2019-04-29 14:18:15 -0700

    re-org

--- text_to_places.py
+++ text_to_places.py
@@ -5,15 +5,69 @@ import pandas as pd
 import geopandas as gpd
 import wikipedia
 from shapely.geometry import Point
+from nltk import ne_chunk, pos_tag, word_tokenize
+from nltk.tree import Tree
 nlp = en_core_web_sm.load()
 
-skip = ['mars', 'earth', '\n', 'xi', 'xv']
+skip = ['thou', 'unthrifty', '\n\n\n                    ', '\n', 'helen', 'state', 'love', 'mars', 'lean', 'amen', 'tyrants', 'wilt', 'duke']
 current_id = 0
 
-def getLocations(text, includesents=True):
-    return getLabeledEntities(text, ['GPE', 'LOC'], includesents)
+def getLocations(text, includesents=True, output=False, model='spacy'):
+    if model=='spacy':
+        return getLabeledEntitiesSpacy(text, ['GPE', 'LOC'], includesents, output)
+    elif model=='nltk':
+        return getLabeledEntitiesNltk(text, ['GPE'])
+    else:
+        raise Exception("could not find model", model)
 
-def getLabeledEntities(text, labels, includesents=True):
+def getLabeledEntitiesNltk(text, labels):
+    labeled = get_continuous_chunks(text)
+    filtered = [entity for entity in labeled if entity[1] in labels]
+    return filtered
+
+def get_continuous_chunks(text):
+    chunked = ne_chunk(pos_tag(word_tokenize(text)))
+    prev = None
+    continuous_chunk = []
+    current_chunk = []
+    current_label = None
+
+    for i,chunk in enumerate(chunked):
+        if type(chunk) == Tree:
+            current_chunk.append(" ".join([token for token, pos in chunk.leaves()]))
+            current_label = chunk.label()
+        elif current_chunk:
+            named_entity = " ".join(current_chunk)
+            if named_entity not in continuous_chunk:
+                continuous_chunk.append([named_entity, current_label])
+                current_chunk = []
+                current_label = None
+        else:
+            continue
+
+    if continuous_chunk:
+        named_entity = " ".join(current_chunk)
+        if named_entity not in continuous_chunk and named_entity:
+            continuous_chunk.append([named_entity])
+
+    return continuous_chunk
+
+def getLabeledEntitiesSpacy(text, labels, includesents=True, output=False):
+    '''
+    Before calling helper function, chunk text if larger than max size.
+    '''
+    entities = []
+    if len(text) > 50_000:
+        for idx in range(0, len(text), 50_000):
+            entities += _getLabeledEntities(text[idx:idx+50_000], labels, includesents)
+            if output:
+                print(f'{int(100*round(idx/len(text), 4))}%', end='\r')
+        print('done with entities')
+    else:
+        entities = _getLabeledEntities(text, labels, includesents)
+    return entities
+
+def _getLabeledEntities(text, labels, includesents=True):
     '''
     Given raw text, outputs a list of lists of the form [text, entity label, word index, sentence (if includesents=True)]
     '''
@@ -24,6 +78,7 @@ def getLabeledEntities(text, labels, includesents=True):
             entities.append([X.text, X.label_, i, X.sent] if includesents else [X.text, X.label_, i])
 
     return entities
+
 
 def getGeojson(locations, locator=geocoder.osm, output=False):
     '''
@@ -36,7 +91,10 @@ def getGeojson(locations, locator=geocoder.osm, output=False):
         if output:
             print(f'{i+1}/{len(locations)}', location_name)
         if location_name not in geojsonDict:
-            geojsonDict[location_name] = locator(location_name.lower()).geojson
+            try:
+                geojsonDict[location_name] = locator(location_name.lower()).geojson
+            except ValueError as err:
+                print(location_name, err)
     return geojsonDict
 
 def getLatLng(locations, locator=geocoder.osm, output=False):
